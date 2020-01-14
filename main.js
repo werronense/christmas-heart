f9f09e37a00b8079272562ecca865fcecd36d466
werronense <31091965+werronense@users.noreply.github.com>
2020-01-14 15:37:46 -0800

    Initialize repository

---
+++ main.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+  const canvas = document.getElementById('mainCanvas');
+  const ctx = canvas.getContext('2d');
+
+  let width = canvas.width = window.innerWidth;
+  let height = canvas.height = window.innerHeight;
+
+}
