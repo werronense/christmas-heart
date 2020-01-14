b6268d2f8595acf811f67b47b9e4a02967cabb72
werronense <31091965+werronense@users.noreply.github.com>
2020-01-14 15:52:41 -0800

    Add drawDiamond function

--- main.js
+++ main.js
@@ -5,4 +5,15 @@ window.onload = () => {
   let width = canvas.width = window.innerWidth;
   let height = canvas.height = window.innerHeight;
 
+  function drawDiamond(startX, startY, sideLength, color) {
+    ctx.beginPath();
+    ctx.moveTo(startX, startY);
+    ctx.lineTo(startX - (sideLength / 2), startY + (sideLength / 2));
+    ctx.lineTo(startX, startY + sideLength);
+    ctx.lineTo(startX + (sideLength / 2), startY + (sideLength / 2));
+    ctx.fillStyle = color;
+    ctx.fill();
+  }
+
+  drawDiamond(100, 0, 200, 'orange');
 }
